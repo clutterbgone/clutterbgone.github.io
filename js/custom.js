219073ed1feefbe7c9d08fab667ed3036e662aba
Cameron Harp <charp5257@gmail.com>
2020-06-12 23:14:24 -0600

    added mobile swipe to gallery

--- js/custom.js
+++ js/custom.js
@@ -30,6 +30,26 @@
         loader.addClass("invisible");
       });
   });
+
+  $carousel.on("touchstart", function(event){
+    var xClick = event.originalEvent.touches[0].pageX;
+
+    $(this).one("touchmove", function(event){
+      var xMove = event.originalEvent.touches[0].pageX;
+      
+      if( Math.floor(xClick - xMove) > 5 ){
+        $(this).carousel('next');
+      }
+      else if( Math.floor(xClick - xMove) < -5 ){
+        $(this).carousel('prev');
+      }
+    });
+  });
+
+  $carousel.on("touchend", function(){
+    $(this).off("touchmove");
+  });
+
 	
 	// Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
